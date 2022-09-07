6697f50b33aa2939bb883abbf858c6378d9b8480
‘ling’ <1300076415@qq.com>
2022-09-07 21:26:41 +0800

    webpack-vue-cli

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1 class="title">hello App</h1>
+    <Home></Home>
+</template>
+
+<script>
+import Home from './views/Home/index.vue'
+export default {
+    name:'App',
+    components:{
+        Home
+    }
+}
+</script>
+
+<style>
+.title {
+    width: 200px;
+    height: 200px;
+    background-color: pink;
+}
+</style>
